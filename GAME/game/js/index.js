ec49b2d946e90cbf7ff43ec1d108d6c97cdfaccd
liuchaojie <liuchaojie@58ganji.com>
2017-12-06 14:04:58 +0800

    game

--- GAME/game/js/index.js
+++ GAME/game/js/index.js
@@ -1,71 +1,47 @@
-var touchBtn1 = document.getElementById("touchBtn1");
-var touchBtn2 = document.getElementById("touchBtn2");
-var touchBtn3 = document.getElementById("touchBtn3");
-touchBtn1.addEventListener("touchstart",function(ev){
+var jsonData =
+    {
+        "challenger": {
+            "name": "肉球猫",
+            "img": "http://image.youkongwan.com/rqdld/cat.png",
+            "invicode": "ABHLT"
+        },
+        "boss": {
+            "name":"12阶苍狼",
+            "img": "http://image.youkongwan.com/rqdld/wolf.png"
+        },
+        "download":"http://h.youkongwan.com/rqdld/index.html"
+    };
+
+var lang = getParamByUrl('lang');
+if((lang == 'false') || (lang == 'zh')){
+    alert('中文');
+}else if(lang == 'zhf'){
+    $('.container .partA .partAImg2').attr('src', 'image/partAImg2_f.png');
+}
+
+//复制功能
+$('#copyBtn').unbind('click').bind('click', function (ev) {
     ev = ev || event;
     if(ev.preventDefault){
         ev.preventDefault();
     }else{
         return false;
     }
-});
-touchBtn1.addEventListener("touchend",function(ev){
-    ev = ev || event;
-    if(ev.preventDefault){
-        ev.preventDefault();
-    }else{
-        return false;
-    }
-    var Url1=document.getElementById("copyCode1");
+    var Url1=document.getElementById("copyCode");
+    console.log(Url1);
     Url1.select(); // 选择对象
     document.execCommand("Copy"); // 执行浏览器复制命令
-    alert('复制成功，小助在等你哦');
+    alert('复制成功!');
     document.documentElement.style.webkitUserSelect = 'none';
     document.documentElement.style.webkitTouchCallout = 'none';
 });
 
-touchBtn2.addEventListener("touchstart",function(ev){
-    ev = ev || event;
-    if(ev.preventDefault){
-        ev.preventDefault();
-    }else{
-        return false;
-    }
-});
-touchBtn2.addEventListener("touchend",function(ev){
-    ev = ev || event;
-    if(ev.preventDefault){
-        ev.preventDefault();
-    }else{
-        return false;
-    }
-    var Url1=document.getElementById("copyCode2");
-    Url1.select(); // 选择对象
-    document.execCommand("Copy"); // 执行浏览器复制命令
-    alert('复制成功，小助在等你哦');
-    document.documentElement.style.webkitUserSelect = 'none';
-    document.documentElement.style.webkitTouchCallout = 'none';
+//前往下载
+$('#downloadBtn').unbind('click').bind('click', function(){
+    alert(1);
 });
 
-touchBtn3.addEventListener("touchstart",function(ev){
-    ev = ev || event;
-    if(ev.preventDefault){
-        ev.preventDefault();
-    }else{
-        return false;
-    }
-});
-touchBtn3.addEventListener("touchend",function(ev){
-    ev = ev || event;
-    if(ev.preventDefault){
-        ev.preventDefault();
-    }else{
-        return false;
-    }
-    var Url1=document.getElementById("copyCode3");
-    Url1.select(); // 选择对象
-    document.execCommand("Copy"); // 执行浏览器复制命令
-    alert('复制成功，小助在等你哦');
-    document.documentElement.style.webkitUserSelect = 'none';
-    document.documentElement.style.webkitTouchCallout = 'none';
+//马上协助
+$('#helpBtn').unbind('click').bind('click', function(){
+    alert(2);
 });
