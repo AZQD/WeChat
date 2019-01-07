a3c30e5e07e648107a1d41455404fe24b2a62cb2
liuchaojie <liuchaojie@58ganji.com>
2019-01-08 00:12:41 +0800

    skeleton/demo

--- Applet/pages/list/list.js
+++ Applet/pages/list/list.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     "pages":[
-      "pages/skeleton/skeleton",
+      "pages/skeleton/demo",
       "pages/skeleton-position/skeleton-position",
       "pages/list/list",
       "pages/index/index",
