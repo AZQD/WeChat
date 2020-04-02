9f32810b0180ecc96d96032ec55a123b59f71159
AZQD <604972395@qq.com>
2020-04-02 16:47:26 +0800

    update

--- shopping/app.js
+++ shopping/app.js
@@ -4,7 +4,16 @@ const PAGESIZE = 10; // 分页列表默认每页展示10条；
 let api = require('./api.js');
 App({
   onLaunch: function () {
-
+    // 只能每个页面分别设置
+    // wx.setNavigationBarColor({
+    //   frontColor: '#000000', // 前景颜色值，仅支持 #ffffff 和 #000000
+    //   backgroundColor: '#bbffaa' // 背景颜色值，有效值为十六进制颜色
+    // });
+    // wx.setTabBarStyle({
+    //   color: '#FF0000', // tab 上的文字默认颜色
+    //   selectedColor: '#00FF00', // tab 上的文字选中时的颜色
+    //   backgroundColor: '#FFFFFF', // tab 的背景色
+    // })
   },
   globalData: {
     wechatId: WECHATID,
