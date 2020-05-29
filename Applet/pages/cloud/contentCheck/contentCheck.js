3c2a402cfc3e6f5e61bb78c98134edcbe550a367
AZQD <604972395@qq.com>
2020-05-29 18:10:15 +0800

    内容发布云数据库

--- Applet/pages/cloud/contentCheck/contentCheck.js
+++ Applet/pages/cloud/contentCheck/contentCheck.js
@@ -6,6 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgSecCheckFlag: true, // 图片校验是否符合规范
     imageArr: []
   },
 
@@ -17,27 +18,30 @@ Page({
   },
 
   msgSecCheckFun(msg){
-    //  调用ContentCheck云函数检查文字是否违规
-    wx.cloud.callFunction({
-      name: 'ContentCheck',
-      data: {
-        msg,
-        // msg: '特3456书yuuo莞6543李zxcz蒜7782法fgnv级完2347全dfji试3726测asad感3847知qwez到',
-      },
-      success(res) {
-        console.log('检查文本内容是否违规：', res.result)
-        if (res.result.errCode === 87014) {
-          wx.showToast({
-            icon: 'none',
-            title: '文字违规',
-          })
-        }else if(res.result.msgR && res.result.msgR.errCode === 0){
-          wx.showToast({
-            title: '文字符合规范',
-          })
+    return new Promise(function (resolve, reject) {
+      //  调用ContentCheck云函数检查文字是否违规
+      wx.cloud.callFunction({
+        name: 'ContentCheck',
+        data: {
+          msg,
+          // msg: '特3456书yuuo莞6543李zxcz蒜7782法fgnv级完2347全dfji试3726测asad感3847知qwez到',
+        },
+        success(res) {
+          console.log('检查文本内容是否违规：', res.result)
+          if (res.result.errCode === 87014) {
+            wx.showToast({
+              icon: 'none',
+              title: '文字违规',
+            })
+          }else if(res.result.msgR && res.result.msgR.errCode === 0){
+            // wx.showToast({
+            //   title: '文字符合规范',
+            // });
+            resolve(true);
+          }
         }
-      }
-    })
+      })
+    });
   },
 
   imgSecCheckFun(img){
@@ -66,7 +70,7 @@ Page({
     });
   },
 
-  chooseImage(){
+  async chooseImage(){
     wx.chooseImage({
       count: 9, // 默认9，最多是九张
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -82,12 +86,15 @@ Page({
 
   async chooseImageAfter (tempFilePaths) {
     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-    let checkImgResult = true;
+    let imgSecCheckFlag = true;
     for (let i = 0; i < tempFilePaths.length; i++) {
-      checkImgResult = await this.imgSecCheckFun(tempFilePaths[i]);
-      if (!checkImgResult) break; // 图片违规，取消渲染；
+      imgSecCheckFlag = await this.imgSecCheckFun(tempFilePaths[i]);
+      if (!imgSecCheckFlag) break; // 图片违规，取消渲染；
     }
-    if(checkImgResult){
+    this.setData({
+      imgSecCheckFlag
+    });
+    if(imgSecCheckFlag){
       this.setData({
         imageArr: tempFilePaths
       });
@@ -102,10 +109,32 @@ Page({
     }
   },
 
-  formSubmit(e) {
+  async formSubmit(e) {
     // console.log('form发生了submit事件，携带数据为：', e.detail.value)
     let inputVal = e.detail.value.input;
-    this.msgSecCheckFun(inputVal);
+    let imageArr = this.data.imageArr;
+    let msgSecCheckFlag = await this.msgSecCheckFun(inputVal);
+    let imgSecCheckFlag = this.data.imgSecCheckFlag;
+
+    if(msgSecCheckFlag && imgSecCheckFlag){
+      const db = wx.cloud.database({});
+      const contentCheckData = db.collection('contentCheckData');
+      contentCheckData.add({
+        data: {
+          inputVal,
+          imageArr,
+          timestamp: Date.now(),
+        },
+        success: function (res) {
+          console.log(res);
+          wx.showModal({
+            title: '成功',
+            content: '成功插入记录'
+          });
+        }
+      });
+    }
+    
   },
 
   formReset(e) {
