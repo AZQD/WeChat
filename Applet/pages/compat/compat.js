f83a0bad8cef7a8f279d7abffc4a2f3688d85816
AZQD <604972395@qq.com>
2022-04-23 11:19:17 +0800

    使用NPMåmock.jså¶åNPM包并引入使用

--- Applet/pages/compat/compat.js
+++ Applet/pages/compat/compat.js
@@ -67,10 +67,10 @@ Page({
         'img': "@image('200x100', '#4A7BF7','#fff','pic')",
         'title': '@ctitle(3,8)',
         'city': "@county(true)",
-        'stock_num': '@integer(0,100)',//库存数量  
+        'stock_num': '@integer(0,100)',//库存数量
         'marketing_start': '@datetime()',
         'marketing_stop': '@now()',
-        'price': '@integer(100,2000)',//现价，单位：分  
+        'price': '@integer(100,2000)',//现价，单位：分
         'original_price': '@integer(100,3000)'
       }]
     });
